e5524b9c3dfc28f985da115ec0c9fed03c1b6765
roice <xingqing_lhtv@qq.com>
2014-11-23 19:29:11 +0800

    Modified to be able to compile on Mac OSX

--- setup.sh
+++ setup.sh
@@ -1,8 +1,30 @@
+####==================================================
+#### Compile, Setup and install script of fgcsgdm
+####==================================================
+
 #!/bin/sh
-cd 3rdparty/fltk-1.3.3
+
+# Get the absolute TOP path of this project
+prjtop=$(cd "$(dirname "$0")"; pwd)
+echo "Absolute path of project top directory is: "$prjtop
+sleep 5
+
+echo "Start Compiling ..."
+sleep 1
+##======== Compile 3rd Party softwares ========
+# Compile fltk
+cd $prjtop/3rdparty/fltk-1.3.3
 mkdir -p build/install
 cd build
 cmake -D CMAKE_INSTALL_PREFIX=./install ..
 make
 make install
+# Compile octave
 
+##======== Compile utils ======================
+# Compile SolPlot3D
+cd $prjtop/utils/solplot
+mkdir build
+cd build
+cmake ..
+make
