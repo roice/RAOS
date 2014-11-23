d7d870391437ecc8591f918f875ffb4406e6dd2c
roice <xingqing_lhtv@qq.com>
2014-11-23 19:27:32 +0800

    Modified to be able to compile on MacOSX

---
+++ uninstall.sh
@@ -0,0 +1,22 @@
+####=================================
+#### Uninstall all build files
+####=================================
+
+#!/bin/sh
+
+# Get project top directory abs path
+prjtop=$(cd "$(dirname "$0")"; pwd)
+echo "Absolute path of project top directory is: "$prjtop
+sleep 2
+
+echo "Start cleaning ..."
+sleep 1
+# clear 3rd party software builds
+cd $prjtop/3rdparty
+rm -rf fltk-1.3.3/build
+
+# clear utils builds
+cd $prjtop/utils
+rm -rf solplot/build
+
+echo "Uninstall done"
