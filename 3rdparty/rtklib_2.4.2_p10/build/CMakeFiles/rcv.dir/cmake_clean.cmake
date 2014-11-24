26052951eedcece8974a4b5a9e760604ff096e1c
roice <xingqing_lhtv@qq.com>
2014-11-24 20:38:32 +0800

    replace rtklib_2.4.2 with rtklib_2.4.2_p10

---
+++ 3rdparty/rtklib_2.4.2_p10/build/CMakeFiles/rcv.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rcv.dir/src/rcv/skytraq.c.o"
+  "CMakeFiles/rcv.dir/src/rcv/novatel.c.o"
+  "CMakeFiles/rcv.dir/src/rcv/ublox.c.o"
+  "CMakeFiles/rcv.dir/src/rcv/ss2.c.o"
+  "CMakeFiles/rcv.dir/src/rcv/crescent.c.o"
+  "CMakeFiles/rcv.dir/src/rcv/gw10.c.o"
+  "CMakeFiles/rcv.dir/src/rcv/javad.c.o"
+  "librcv.pdb"
+  "librcv.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/rcv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 3rdparty/rtklib_2.4.2_p10/build/CMakeFiles/rtk.dir/DependInfo.cmake
@@ -0,0 +1,60 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/src/convkml.c" "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/build/CMakeFiles/rtk.dir/src/convkml.c.o"
+  "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/src/convrnx.c" "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/build/CMakeFiles/rtk.dir/src/convrnx.c.o"
+  "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/src/datum.c" "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/build/CMakeFiles/rtk.dir/src/datum.c.o"
+  "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/src/download.c" "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/build/CMakeFiles/rtk.dir/src/download.c.o"
+  "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/src/ephemeris.c" "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/build/CMakeFiles/rtk.dir/src/ephemeris.c.o"
+  "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/src/geoid.c" "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/build/CMakeFiles/rtk.dir/src/geoid.c.o"
+  "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/src/ionex.c" "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/build/CMakeFiles/rtk.dir/src/ionex.c.o"
+  "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/src/lambda.c" "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/build/CMakeFiles/rtk.dir/src/lambda.c.o"
+  "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/src/options.c" "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/build/CMakeFiles/rtk.dir/src/options.c.o"
+  "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/src/pntpos.c" "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/build/CMakeFiles/rtk.dir/src/pntpos.c.o"
+  "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/src/postpos.c" "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/build/CMakeFiles/rtk.dir/src/postpos.c.o"
+  "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/src/ppp.c" "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/build/CMakeFiles/rtk.dir/src/ppp.c.o"
+  "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/src/ppp_ar.c" "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/build/CMakeFiles/rtk.dir/src/ppp_ar.c.o"
+  "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/src/preceph.c" "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/build/CMakeFiles/rtk.dir/src/preceph.c.o"
+  "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/src/qzslex.c" "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/build/CMakeFiles/rtk.dir/src/qzslex.c.o"
+  "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/src/rcvraw.c" "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/build/CMakeFiles/rtk.dir/src/rcvraw.c.o"
+  "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/src/rinex.c" "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/build/CMakeFiles/rtk.dir/src/rinex.c.o"
+  "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/src/rtcm.c" "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/build/CMakeFiles/rtk.dir/src/rtcm.c.o"
+  "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/src/rtcm2.c" "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/build/CMakeFiles/rtk.dir/src/rtcm2.c.o"
+  "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/src/rtcm3.c" "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/build/CMakeFiles/rtk.dir/src/rtcm3.c.o"
+  "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/src/rtcm3e.c" "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/build/CMakeFiles/rtk.dir/src/rtcm3e.c.o"
+  "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/src/rtkcmn.c" "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/build/CMakeFiles/rtk.dir/src/rtkcmn.c.o"
+  "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/src/rtkpos.c" "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/build/CMakeFiles/rtk.dir/src/rtkpos.c.o"
+  "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/src/rtksvr.c" "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/build/CMakeFiles/rtk.dir/src/rtksvr.c.o"
+  "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/src/sbas.c" "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/build/CMakeFiles/rtk.dir/src/sbas.c.o"
+  "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/src/solution.c" "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/build/CMakeFiles/rtk.dir/src/solution.c.o"
+  "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/src/stream.c" "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/build/CMakeFiles/rtk.dir/src/stream.c.o"
+  "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/src/streamsvr.c" "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/build/CMakeFiles/rtk.dir/src/streamsvr.c.o"
+  "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/src/tle.c" "/home/roice/workspace/ExPlatFormDevelop/fgcsgdm/3rdparty/rtklib_2.4.2_p10/build/CMakeFiles/rtk.dir/src/tle.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "ENACMP"
+  "ENAGAL"
+  "ENAGLO"
+  "ENAQZS"
+  "EXTLEX"
+  "NEXOBS=3"
+  "NFREQ=3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
