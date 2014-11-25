2bfbd5240e23092f86549f8dbd4645fd2b3fe26e
roice <xingqing_lhtv@qq.com>
2014-11-25 23:05:16 +0800

    delete octave, use debian installed octave

--- 3rdparty/octave-3.8.2/liboctave/cruft/Faddeeva/module.mk
+++
@@ -1,3 +0,0 @@
-EXTRA_DIST += Faddeeva/module.mk
-
-libcruft_la_SOURCES += Faddeeva/Faddeeva.cc Faddeeva/Faddeeva.hh
--- build.sh
+++ build.sh
@@ -9,7 +9,8 @@ prjtop=$(cd "$(dirname "$0")"; pwd)
 echo "Absolute path of project top directory is: "$prjtop
 sleep 5
 
-echo "Start Compiling ..."
+echo "Start Compiling 3d party soft ..."
+echo "Start Compiling FLTK..."
 sleep 1
 ##======== Compile 3rd Party softwares ========
 # Compile fltk
@@ -19,6 +20,9 @@ cd build
 cmake -D CMAKE_INSTALL_PREFIX=./install ..
 make
 make install
+
+echo "Start Compiling RTKLIB..."
+sleep 1
 # Compile rtklib, after make, there will be librtk.a and librcv.a in the
 # build directory
 cd $prjtop/3rdparty/rtklib_2.4.2_p10
@@ -26,8 +30,9 @@ mkdir build
 cd build
 cmake ..
 make
-# Compile octave
 
+echo "Start Compiling utils"
+sleep 5
 ##======== Compile utils ======================
 # Compile SolPlot3D
 cd $prjtop/utils/solplot
