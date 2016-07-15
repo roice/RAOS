f280b827950dd4ecdac5b0f16b295d543cbe284d
roice <xingqing_lhtv@qq.com>
2016-07-15 09:28:24 +0800

    fixed paper-rev

--- utils/paper-rev/original.py
+++ utils/paper-rev/original.py
@@ -26,25 +26,25 @@ pos_17_under_AOE = h5py.File('Record_2016-07-08_15-38-16.h5', 'r+')['enu_of_robo
 
 # get data
 rd_13_no_AOE = 3.3 - rd_13_no_AOE[75:440, 0]
-pos_13_no_AOE = - pos_13_no_AOE[75:440, 1]
+pos_13_no_AOE = - pos_13_no_AOE[75:440, 1] - 1.2
 rd_14_no_AOE = 3.3 - rd_14_no_AOE[85:490, 0]
-pos_14_no_AOE = - pos_14_no_AOE[85:490, 1]
+pos_14_no_AOE = - pos_14_no_AOE[85:490, 1] - 1.2
 rd_15_no_AOE = 3.3 - rd_15_no_AOE[85:490, 0]
-pos_15_no_AOE = - pos_15_no_AOE[85:490, 1]
+pos_15_no_AOE = - pos_15_no_AOE[85:490, 1] - 1.2
 rd_16_no_AOE = 3.3 - rd_16_no_AOE[155:390, 0]
-pos_16_no_AOE = - pos_16_no_AOE[155:390, 1]
+pos_16_no_AOE = - pos_16_no_AOE[155:390, 1] - 1.2
 rd_17_no_AOE = 3.3 - rd_17_no_AOE[145:350, 0]
-pos_17_no_AOE = - pos_17_no_AOE[145:350, 1]
+pos_17_no_AOE = - pos_17_no_AOE[145:350, 1] - 1.2
 rd_13_under_AOE = 3.3 - rd_13_under_AOE[348:445, 0]
-pos_13_under_AOE = - pos_13_under_AOE[348:445, 1]
+pos_13_under_AOE = - pos_13_under_AOE[348:445, 1] - 1.2
 rd_14_under_AOE = 3.3 - rd_14_under_AOE[359:450, 0]
-pos_14_under_AOE = - pos_14_under_AOE[359:450, 1]
+pos_14_under_AOE = - pos_14_under_AOE[359:450, 1] - 1.2
 rd_15_under_AOE = 3.3 - rd_15_under_AOE[569:661, 0]
-pos_15_under_AOE = - pos_15_under_AOE[569:661, 1]
+pos_15_under_AOE = - pos_15_under_AOE[569:661, 1] - 1.2
 rd_16_under_AOE = 3.3 - rd_16_under_AOE[135:223, 0]
-pos_16_under_AOE = - pos_16_under_AOE[135:223, 1]
+pos_16_under_AOE = - pos_16_under_AOE[135:223, 1] - 1.2
 rd_17_under_AOE = 3.3 - rd_17_under_AOE[363:449, 0]
-pos_17_under_AOE = - pos_17_under_AOE[363:449, 1]
+pos_17_under_AOE = - pos_17_under_AOE[363:449, 1] - 1.2
 
 fig = plt.figure(figsize=(12,10))
 ax = fig.add_subplot(111)
@@ -60,7 +60,7 @@ ax.plot(pos_15_no_AOE, rd_15_no_AOE, 'r--', label='0 m, no AOE')
 ax.plot(pos_16_no_AOE, rd_16_no_AOE, 'y--', label='0.1 m, no AOE')
 ax.plot(pos_17_no_AOE, rd_17_no_AOE, 'k--', label='0.2 m, no AOE')
 
-ax.set_xlabel("Position (m)")
+ax.set_xlabel("Y component of position (m)")
 ax.set_ylabel("Gas sensor reading (V)")
 ax.legend()
 
